c415ba5fcd44307aec9f3039bca17e2975e6f5a2
Chris Reuter <chris@blit.ca>
2013-09-04 19:23:34 -0400

    Nothing useful, just more script bashing.

--- misc/rungd.sh
+++ misc/rungd.sh
@@ -5,27 +5,28 @@
 set -e
 
 #VG=valgrind tool=callgrind
-#VG="gdb --args"
+VG="gdb --args"
 
 GDDIR=../../gd-libgd/src
 GDBLD_DIR=$GDDIR/.libs/
 
 cd `dirname $0`
 
-pushd ../../gd-libgd
-if make -q; then
-    echo "Lib is up to date."
-else
-#    make clean
-    make -j 4
+# pushd ../../gd-libgd/src
+# if make -q; then
+#     echo "Lib is up to date."
+# else
+#     cd ..
+# #    make clean
+#     make -j 4
 
-    echo
-    echo
-    echo "Installing:"
-    sudo make install
-    sudo ldconfig
-fi
-popd
+#     echo
+#     echo
+#     echo "Installing:"
+#     sudo make install
+#     sudo ldconfig
+# fi
+# popd
 
 cd ../c_tests/
 
@@ -33,7 +34,7 @@ cd ../c_tests/
 gcc -g -O -Wall `pkg-config gdlib --libs --cflags` gd_resize.c timer.c util.c -lgd -o gd_resize 
 
 #export LD_LIBRARY_PATH=../../
-$VG ./gd_resize ../data/pic1.jpg out 2080 2080 # 2080 2080 2080 
+$VG ./gd_resize ../data/8s.jpg out 2080 2080 # 2080 2080 2080 
 
 
 
