dfce3183d43a281910b15fff6403e3f42f1b9613
Chris Reuter <chris@blit.ca>
2013-09-06 11:22:02 -0400

    Added -lm to compile statement so that it builds on Ubuntu 13.

--- misc/rungd.sh
+++ misc/rungd.sh
@@ -31,7 +31,9 @@ cd `dirname $0`
 cd ../c_tests/
 
 #gcc -g -O -Wall -I$GDDIR  -L$GDBLD_DIR gd_resize.c timer.c util.c -lgd -o gd_resize 
-gcc -g -O -Wall `pkg-config gdlib --libs --cflags` gd_resize.c timer.c util.c -lgd -o gd_resize 
+gcc -g -O -Wall `pkg-config gdlib --libs --cflags` \
+    gd_resize.c timer.c util.c \
+    -lgd -lm -o gd_resize 
 
 #export LD_LIBRARY_PATH=../../
 $VG ./gd_resize ../data/8s.jpg out 2080 2080 # 2080 2080 2080 
