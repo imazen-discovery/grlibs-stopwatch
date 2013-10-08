e326a4d440b99d7dcbad2d965e9a197fdfe3733d
Chris Reuter <chris@blit.ca>
2013-10-07 21:23:29 -0400

    Checkpoint.  gd_whiteblock.rb script works; had to fix nightmarish cfg problem.

--- misc/gd_whiteblock.rb
+++ misc/gd_whiteblock.rb
@@ -7,11 +7,22 @@
 require 'gd2-ffij'
 include GD2
 
-im = Image::TrueColor.new(300, 300)
+X=300
+Y=300
+
+im = Image::TrueColor.new(X, Y)
+
+im.draw{|canvas|
+  canvas.color = Color[1.0, 1.0, 1.0]
+  canvas.rectangle(0, 0, X-1, Y-1, true)
+}
+im.export("before.png")
 
 im.interpolation_method = GD_BICUBIC
-im.resizeInterpolated(300, 600)
+im2 = im.resizeInterpolated(2*X, Y)
+im2.export("middle.png")
 
-im.export("result.png")
+im3 = im2.resizeInterpolated(X, Y)
+im3.export("after.png")
 
 
